e07c24a6aa7f7dbcb2462b6b1136c230348578a8
Zachary Murray <89foxlx@gmail.com>
2020-02-14 22:41:18 -0600

    added axios call. added search feature. added some of render function

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            users: [],
+            userText: ""
+        };
+    }
+
+    componentDidMount() {
+        axios.get("https://api.github.com/users/ZachM89")
+            .then(res => {//res.json()
+                console.log(res);
+            })
+            .then(users => this.setState({ users: users.message }))
+            .catch(err => console.log("Data not found", err));
+    }
+
+    handleChanges = e => {
+        this.setState({ userText: e.target.value });
+        console.log(e.target.value);
+    }
+
+    componentDidUpdate(prevState) {
+        if (prevState.users !== this.state.users) {
+            console.log("Users have changed");
+
+            //if (this.state.userText === ")
+        }
+    }
+
+    fetchUsers = e => {
+        e.preventDefault();
+
+        axios.get(`https://api.github.com/users/${this.state.userText}`)
+            .then(res => {
+                console.log(res);
+            })
+            .then(users => this.setState({ users: users.message }))
+            .catch(err => console.log("No data", err));
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>GitHub Users</h1>
+                    <input
+                        type="text"
+                        value={this.state.userText}
+                        onChange={this.handleChanges}
+                    />
+                <button onClick={this.fetchUsers}>Fetch Users</button>
+                <div>
+                    {this.state.users.map(user => (
+                        <img width="200" src={user} key={user} alt={user} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
