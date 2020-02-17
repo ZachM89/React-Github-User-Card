885fbf1bb58b8ea1b8b8fe53a8d3846fc54fc7be
Zachary Murray <89foxlx@gmail.com>
2020-02-17 02:20:20 -0600

    added minor styling. displaying followers

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -7,7 +7,9 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            users: ['one','two','three'],
+            users: [],
+            followers: [],
+            followerMsg: ''
         };
     }
 
@@ -22,32 +24,50 @@ class App extends React.Component {
         // console.log(e.target.value);
     }
 
-    testing = e => {
-        e.preventDefault();
-        console.log(this.state.userText);
-        axios.get(`https://api.github.com/users/${this.state.userText}`)
-        .then( res => {
-            //console.log(this.state.userText);
-            console.log(res.data);
-            this.setState({users: [res.data]})
-        })
-        console.log(this.state.users);
+    componentDidUpdate(prevState) {
+        console.log("componentDidUpdate running");
 
-        //this.setState({users: ['test']});
+        if (prevState.dogs !== this.state.dogs) {
+            console.log("Dogs have changed :)");
+
+            if (this.state.dogText === "pomeranian") {
+                console.log("pomeranians :(");
+
+                fetch("https://dog.ceo/api/breed/husky/images")
+                .then(res => res.json())
+                .then(dogs => this.setState({ dogText: "husky", dogs: dogs.message }))
+                .catch(err => console.log("No dogs :(", err));
+            }
+        }
     }
 
-
-    // fetchUsers = e => {
-    //     // e.preventDefault();
-
-    //     axios.get(`https://api.github.com/users/raajnpatel`)
-    //         .then(res => {
-    //             console.log(res.data)
-    //             this.setState({ users: res.data })
-    //             console.log(this.state.users);
-    //         })
-    //         .catch(err => console.log("No data", err));
-    // };
+    fetchUsers = e => {
+        e.preventDefault();
+        //console.log(this.state.userText);
+        axios.get(`https://api.github.com/users/${this.state.userText}`)
+            .then( res => {
+                //console.log(this.state.userText);
+                console.log(res.data);
+                this.setState({users: [res.data]})
+                
+        })
+        axios.get(`https://api.github.com/users/${this.state.userText}/followers`)
+            .then( res=> {
+                console.log(res);
+                this.setState({followers: res.data})
+                if(this.state.followers.length > 0) {
+                    console.log(this.state.followers.length)
+                    if(this.state.followers.length === 1){
+                        this.setState({follwerMsg: `Mr popular over here with his ${this.state.followers.length} follower`})
+                    } else {
+                        this.setState({follwerMsg: `Mr popular over here with his ${this.state.followers.length} followers`})
+                    }
+                } else {
+                    this.setState({follwerMsg: 'LOL this guy has no friends'})
+                }
+            })
+        //console.log(this.state.users);
+    }
 
     render() {
         return (
@@ -59,22 +79,37 @@ class App extends React.Component {
                         value={this.state.userText}
                         onChange={this.handleChanges}
                     />
-                    <button onClick={this.testing}>Fetch Users</button>
+                    <button onClick={this.fetchUsers}>Fetch Users</button>
                 </div>
                 <div className="card-container">
+                    {console.log(this.state.followers)}
                     {this.state.users.map(user => {
-                            console.log(user)
-                            return (
-                                <div className="user-card">
-                                    <p>{user.login}</p>
-                                    <p>{user.name}</p>
-                                    <p>{user.location}</p>
-                                </div>
-                            )
-                        })}
+                        //console.log(user)
+                        return (
+                            <div className="user-card">
+                                <img src={user.avatar_url} alt={user.avatar_url}/> 
+                                <p>{user.login}</p>
+                                <p>{user.name}</p>
+                                <p>{user.location}</p>
+                            </div>
+                        )
+                    })}
+                    
                 </div>
+                
+                <h3>{this.state.follwerMsg}</h3>
+                <div className="followers-container">
+                    {console.log(this.state.followers.data)}
                     
-                    
+                    {this.state.followers.map(follower => {
+                        return (
+                            <div>
+                                
+                                <p>{follower.login}</p>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         );
     }
